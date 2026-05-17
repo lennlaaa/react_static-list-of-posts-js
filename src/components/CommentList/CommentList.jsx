9d88fceda428b08c37cf623928056c00fb8a6b26
Olena <simonaolena6@gmail.com>
2026-05-17 20:06:06 +0300

    I ,ade CommentList

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,7 @@
-export const CommentList = () => <>Put the list here</>;
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map( comment =>
+    <Comment key={comment.id} comment={comment}></Comment>
+    )}
+    </div>
+);
