b6af0499cf7019f39e0d0aa84971491271609dec
Olena <simonaolena6@gmail.com>
2026-05-17 19:50:57 +0300

    I made PostInfo

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,17 @@
-export const PostInfo = () => <>Put the post here</>;
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <h3>{post.title}</h3>
+
+    <p>{post.body}</p>
+
+    <p>
+      Author: {post.user.name} {post.user.email}
+    </p>
+
+    <div className="PostInfo__comments">
+      {post.comments.map(comment => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </div>
+  </div>
+);
