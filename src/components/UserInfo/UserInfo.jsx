9f25196b467fa9ee704037ee1a535eb5af3cf837
Olena <simonaolena6@gmail.com>
2026-05-19 18:11:46 +0300

    I made UserInfo

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,10 @@
-export const UserInfo = () => <>Put the user here</>;
+export const UserInfo = ({ user }) => (
+  <div className="UserInfo">
+    <div className="UserInfo__name">
+      {user.name}
+    </div>
+    <div className="UserInfo__email">
+      {user.email}
+    </div>
+  </div>
+);
